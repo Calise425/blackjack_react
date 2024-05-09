54f664b0522945b8e95772cdf9b495018d81a535
Calise425 <caitlin.norris1998@gmail.com>
2024-05-09 16:06:15 -0600

    Updated file structure, added pictures, and connected html/css/js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const BlackJack = () => {
+  return <h1>Hello World</h1>;
+};
+
+ReactDOM.render(<BlackJack />, document.getElementById("app"));
